5b14b7b4e7a1c54f0e626a391a5edf511f0a7b7e
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2025-01-25 20:36:56 +0800

    Handle undefined and null

--- index.js
+++ index.js
@@ -3,27 +3,32 @@
   var TRUTHY_VALUES = 'y yes true'.split(/\s/);
 
   function toBoolean(value) {
+    // undefined and null are considered falsy values
+    if (value === undefined || value === null) {
+      return false;
+    }
+
     value = value.toString();
     value = value.trim();
     value = value.toLowerCase();
 
     // Empty string is considered a falsy value
-    if(!value.length) {
+    if (!value.length) {
       return false;
 
     // Any number above zero is considered a truthy value
-    } else if(!isNaN(Number(value))) {
+    } else if (!isNaN(Number(value))) {
       return value > 0;
 
     // Any value not marked as a truthy value is automatically falsy
     } else {
-      return TRUTHY_VALUES.indexOf(value) >= 0;
+      return TRUTHY_VALUES.includes(value);
     }
   }
 
-  if(typeof define === 'function' && define.amd) {
+  if (typeof define === 'function' && define.amd) {
     define(function() { return toBoolean; });
-  } else if(typeof module !== 'undefined' && module.exports) {
+  } else if (typeof module !== 'undefined' && module.exports) {
     module.exports = toBoolean;
   } else {
     exports.toBoolean = toBoolean;
