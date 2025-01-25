6b7b3800ca30accba938ced124a6b6c2d8501a9c
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2025-01-25 21:26:02 +0800

    Package as module

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,35 @@
 declare var define: any;
 
-(function(exports) {
+const TRUTHY_VALUES = 'y yes true'.split(/\s/);
 
-  var TRUTHY_VALUES = 'y yes true'.split(/\s/);
-
-  function toBoolean(value: any): boolean {
-    // undefined and null are considered falsy values
-    if (value === undefined || value === null) {
-      return false;
-    }
-
-    value = value.toString();
-    value = value.trim();
-    value = value.toLowerCase();
-
-    // Empty string is considered a falsy value
-    if (!value.length) {
-      return false;
-
-    // Any number above zero is considered a truthy value
-    } else if (!isNaN(Number(value))) {
-      return value > 0;
-
-    // Any value not marked as a truthy value is automatically falsy
-    } else {
-      return TRUTHY_VALUES.includes(value);
-    }
+function toBoolean(value: any): boolean {
+  // undefined and null are considered falsy values
+  if (value === undefined || value === null) {
+    return false;
   }
 
-  if (typeof define === 'function' && define.amd) {
-    define(function() { return toBoolean; });
-  } else if (typeof module !== 'undefined' && module.exports) {
-    module.exports = toBoolean;
+  value = value.toString();
+  value = value.trim();
+  value = value.toLowerCase();
+
+  // Empty string is considered a falsy value
+  if (!value.length) {
+    return false;
+
+  // Any number above zero is considered a truthy value
+  } else if (!isNaN(Number(value))) {
+    return value > 0;
+
+  // Any value not marked as a truthy value is automatically falsy
   } else {
-    exports.toBoolean = toBoolean;
+    return TRUTHY_VALUES.includes(value);
   }
+}
 
-})(this);
+if (typeof define === 'function' && define.amd) {
+  define(function() { return toBoolean; });
+} else if (typeof module !== 'undefined' && module.exports) {
+  module.exports = toBoolean;
+}
+
+export default toBoolean;
